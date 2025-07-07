92fbfa48928db0ebe4dc7ed9c339589a32563ae1
Rodrigo <83593684+NiggetChuckens@users.noreply.github.com>
2025-07-06 23:17:07 -0400

    Update main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -572,4 +572,4 @@ if __name__ == '__main__':
     db.populate_test_data.populate_test_data()
     print("Starting Flask server...") 
 
-    app.run(debug=True, port='5000', host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
